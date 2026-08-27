cb107910b406ba6ce7cbcf5c6f70330b55fc7f11
Ahmet Özcan <ozcanahmet973@gmail.com>
2026-08-27 04:18:25 +0300

    Stage 0: setup server and supabase client

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
+const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 const port = 3000;
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const swaggerUi = require('swagger-ui-express');
 const openapiDocument = require('./openapi.json');
