a2a72d4664bd9378c7f40543a59e83147c2c51cc
Ahmet Özcan <ozcanahmet973@gmail.com>
2026-08-27 04:47:32 +0300

    Stage 2: public route and unverified protected route

--- index.js
+++ index.js
@@ -57,6 +57,29 @@ app.get('/health', (req, res) => {
   res.json({ "status": "ok" });
 });
 
+// PUBLIC & PROTECTED ENDPOINTS =======================================================================
+// GET /public/info
+app.get('/public/info', (req, res) => {
+  res.status(200).json({ message: "Welcome stranger! This info is public." });
+});
+
+// GET /protected/profile
+app.get('/protected/profile', (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: "Access token required" });
+  }
+
+  const token = authHeader.split(' ')[1];
+  
+  if (!token) {
+    return res.status(401).json({ error: "Access token required" });
+  }
+
+  res.status(200).json({ message: "You provided a token!"});
+});
+
 // AUTH ENDPOINTS =====================================================================================
 // POST /auth/signup
 app.post('/auth/signup', async (req, res) => {
